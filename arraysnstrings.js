d0df012480a45abf740b39e6e8897c6f6a2429bc
Shivi7434 <shivalikhanuja.net@gmail.com>
2017-12-02 00:16:39 -0600

    adding boolean primeNaive

---
+++ arraysnstrings.js
@@ -0,0 +1,14 @@
+//Random Algos
+//boolean primeaive
+
+boolean primeNaive(int n) {
+    if (n < 2 ){
+        return false;
+    } 
+    for (int i = 2; i < n; i++){
+        if (n % i == 0){
+            return false;
+        }
+    }    
+    return true;
+}
