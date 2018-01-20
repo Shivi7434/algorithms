eb5c1fbdac9b70518c0b5ca9020ed05872444371
Shivi7434 <shivalikhanuja.net@gmail.com>
2018-01-20 15:45:53 -0600

    algo parctise

--- algoinjavascript.js
+++ algoinjavascript.js
@@ -20,5 +20,22 @@ addandLog (['A','B','C']);
 addandLog (['A','D','C','B']);
 
 /////////////////////////
+// to check number of times word has been repeated//
+function harmlessRansomNote (noteText, magazineText){
+  var noteArr = noteText.split(' ');
+  var magazineArr = magazineText.split(' ');
+  var magazineObj = {};
+  
+  magazineArr.forEach(word => {
+    if (!magazineObj[word]) magazineObj[word] = 0;
+    magazineObj[word]++;
+  });
+  
+  console.log(magazineObj);
+}
+
+harmlessRansomNote('','this is all the magazine text in the magazine ');
+////////////////////////////////////
+
 
   
