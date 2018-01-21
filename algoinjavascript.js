21d41ae69ab38cba27b0007c29fb7864da08ef6e
Shivi7434 <shivalikhanuja.net@gmail.com>
2018-01-21 16:41:34 -0600

    algo practise

--- algoinjavascript.js
+++ algoinjavascript.js
@@ -84,3 +84,30 @@ function isPalindrome (string) {
 console.log(isPalindrome("Madam I'm Adam"));
 
 ///////////////////////////////
+//// Ceaser Cipher//// result in movin 1 alphabet forward and converting into it
+function caesarCipher (str, num ) {
+  num = num % 26;
+  var lowerCaseString = str.toLowerCase();
+  var alphabet = 'abcdefghijlkmnopqrstuvwxyz'.split('');
+  var newString = '';
+  for ( var i = 0; i < lowerCaseString.length; i++) {
+    var currentLetter = lowerCaseString[i];
+    
+    if (currentLetter === ' ') {
+      newString += currentLetter;
+      continue;
+    }
+    var currentIndex = alphabet.indexOf(currentLetter);
+    var newIndex = currentIndex + num;
+    if (newIndex > 25 ) newIndex = newIndex - 26;
+    if (newIndex < 0) newIndex = 26 + newIndex;
+    if (str[i] === str[i].toUpperCase()){
+      newString += alphabet[newIndex].toUpperCase();
+    }  
+    
+    else newString += alphabet[newIndex];
+  }
+  return newString;
+}
+caesarCipher('Zoo Keeper', 1)
+///// results movind forward by 1 - 'App Mffqfs'//////
